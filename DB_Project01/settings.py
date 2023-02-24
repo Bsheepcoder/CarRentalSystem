77b6fa51435cc20c550499fd84556342a341e483
husile <710418455@qq.com>
2023-02-25 03:39:54 +0800

    修改setting

--- DB_Project01/settings.py
+++ DB_Project01/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = '/home/yanfriends_server/static'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
